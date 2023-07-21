7a774c7a24f1de4bdaf4607f093fb54e11bb46fb
AminaBoudj <135740102+AminaBoudj@users.noreply.github.com>
2023-07-21 10:29:58 +0200

    Update test_api_test.py

--- test_api_test.py
+++ test_api_test.py
@@ -4,42 +4,42 @@ import pytest
 import requests
 
 def test_index():
-    response = requests.get('http://127.0.0.1:80/')
+    response = requests.get('http://amina1984.pythonanywhere.com')
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello, stranger'}
 
 def test_score_min():
-    response = requests.get('http://127.0.0.1:80/score_min/')
+    response = requests.get('http://amina1984.pythonanywhere.com/score_min/')
     assert response.status_code == 200
     assert response.json() == {"score_min" : 0.55}
 
 def test_predict():
     client_id = 100001
-    response = requests.get(f'http://127.0.0.1:80/predict?client_id={client_id}')
+    response = requests.get(f'http://amina1984.pythonanywhere.com/predict?client_id={client_id}')
     assert response.status_code == 200
     assert "proba" in response.json()
     assert isinstance(response.json()["proba"], float)
 
 def test_feats():
-    response = requests.get('http://127.0.0.1:80/feats/')
+    response = requests.get('http://amina1984.pythonanywhere.com/feats/')
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
-def test_importances():
+"""def test_importances():
     client_id = 100001
     response = requests.get(f'http://127.0.0.1:80/importances?client_id={client_id}')
     assert response.status_code == 200
     assert isinstance(response.json(), list)
-
+"""
 def test_bar():
     client_id = 100001
     feature = "EXT_SOURCE_3"
-    response = requests.get(f'http://127.0.0.1:80/bar?client_id={client_id}&feature={feature}')
+    response = requests.get(f'http://amina1984.pythonanywhere.com/bar?client_id={client_id}&feature={feature}')
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
 def test_boxplot():
     feature = "EXT_SOURCE_3"
-    response = requests.get(f'http://127.0.0.1:80/boxplot?feature={feature}')
+    response = requests.get(f'http://amina1984.pythonanywhere.com/boxplot?feature={feature}')
     assert response.status_code == 200
     assert isinstance(response.json(), list)
